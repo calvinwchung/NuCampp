71123e954da522e33f808e8a29d50174518138fe
calvin chung <calvin.w.chung@gmail.com>
2022-06-11 11:42:38 -0400

    removing js from path

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -1,4 +1,4 @@
-import CampsitesList from './features/campsites/CampsitesList.js';
+import CampsitesList from './features/campsites/CampsitesList';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
