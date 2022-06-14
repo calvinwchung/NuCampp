8d9b0af14fd3cd850a6df4c24746c0ca137d523d
calvin chung <calvin.w.chung@gmail.com>
2022-06-14 11:14:51 -0400

    got the code to work by closing vscode and relaunching

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -1,16 +1,16 @@
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <CampsitesDirectoryPage />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <CampsitesDirectoryPage />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
