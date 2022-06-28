811577d0c976194012cbf0a1f4b0ca293341a628
calvin chung <calvin.w.chung@gmail.com>
2022-06-28 14:01:43 -0400

    workshop week 4

--- 3-React/nucampsite/src/app/store.js
+++ 3-React/nucampsite/src/app/store.js
@@ -4,7 +4,7 @@ import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 import logger from 'redux-logger';
-
+import { userReducer } from '../features/user/userSlice';
 
 
 
@@ -13,7 +13,8 @@ export const store = configureStore({
     campsites: campsitesReducer,
     comments: commentsReducer,
     promotions: promotionsReducer,
-    partners: partnersReducer
+    partners: partnersReducer,
+    user: userReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
