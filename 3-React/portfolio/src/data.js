cfa5d8257931187f7c4397b4f3065be58bc5513c
calvin chung <calvin.w.chung@gmail.com>
2022-07-19 21:34:37 -0400

    adding cards

---
+++ 3-React/portfolio/src/data.js
@@ -0,0 +1,35 @@
+import Bolivian from '../src/img/bolivian.png'
+
+export const products = [
+  {
+    id: 1,
+    img: Bolivian,
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "http://lama.dev",
+  },
+];
+
