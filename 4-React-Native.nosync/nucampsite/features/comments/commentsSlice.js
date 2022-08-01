0048aec1cf7dcf478eb7bd1e8d3ca1ab3e8800c9
calvin chung <calvin.w.chung@gmail.com>
2022-08-01 10:57:20 -0400

    adding delete function

--- 4-React-Native.nosync/nucampsite/features/comments/commentsSlice.js
+++ 4-React-Native.nosync/nucampsite/features/comments/commentsSlice.js
@@ -31,9 +31,7 @@ const commentsSlice = createSlice({
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
     reducers: {
         addComment: (state, action) => {
-           
             state.commentsArray.push(action.payload);
-            
         }
     },
     extraReducers: {
