f328ca5f607d176299ef931502ac4830553f2bc2
calvin chung <calvin.w.chung@gmail.com>
2022-08-10 12:18:14 -0400

    wifi noti

--- 4-React-Native.nosync/nucampsite/screens/MainComponent.js
+++ 4-React-Native.nosync/nucampsite/screens/MainComponent.js
@@ -1,4 +1,4 @@
-import { Image, Text, Platform, StyleSheet, View } from 'react-native';
+import { Image, Text, Platform, StyleSheet, View, Alert, ToastAndroid } from 'react-native';
 import Constants from 'expo-constants';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 import DirectoryScreen from './DirectoryScreen';
@@ -9,7 +9,7 @@ import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
 import { Icon } from 'react-native-elements';
 import logo from '../assets/images/logo.png';
-import { useDispatch } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchPartners } from '../features/partners/partnersSlice';
 import { fetchCampsites } from '../features/campsites/campsitesSlice';
@@ -18,6 +18,8 @@ import { fetchComments } from '../features/comments/commentsSlice';
 import ReservationScreen from './ReservationScreen';
 import FavoritesScreen from './FavoritesScreen';
 import LoginScreen from './LoginScreen';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/core';
+import NetInfo from '@react-native-community/netinfo';
 
 const Drawer = createDrawerNavigator();
 
@@ -181,10 +183,16 @@ const LoginNavigator = () => {
       <Stack.Screen 
         name='Login'
         component={LoginScreen}
-        options={({navigation}) => ({ 
+        options={({navigation, route}) => ({ 
+          headerTitle: getFocusedRouteNameFromRoute(route),
           headerLeft: () => (
             <Icon 
-              name='sign-in'
+              name={
+                getFocusedRouteNameFromRoute(route) === 
+                  'Register'
+                    ? 'user-plus'
+                    : 'sign-in'
+              }
               type='font-awesome'
               iconStyle={styles.stackIcon}
               onPress={() => navigation.toggleDrawer()}
@@ -219,6 +227,50 @@ const Main = () => {
     dispatch(fetchPartners());
     dispatch(fetchComments());
   }, [dispatch]);
+
+  useEffect(() => {
+    NetInfo.fetch().then((connectionInfo) => {
+      Platform.OS === 'ios'
+        ? Alert.alert(
+          'Initial Network Connectivity Type:', 
+          connectionInfo.type
+        )
+        : ToastAndroid.show(
+          'Initial Network Connectivity Type: ' +
+            connectionInfo.type,
+          ToastAndroid.LONG
+        );
+    });
+
+    const unsubscribeNetInfo = NetInfo.addEventListener(
+        (connectionInfo) => {
+          handleConnectivityChange(connectionInfo);
+        }
+      )
+
+      return unsubscribeNetInfo;
+  }, []);
+
+  const handleConnectivityChange = (connectionInfo) => {
+    let connectionMsg = 'You are now connected to an active network.';
+    switch (connectionInfo.type) {
+      case 'none':
+        connectionMsg = 'No network connection is active.';
+        break;
+      case 'unknown':
+        connectionMsg = 'The network connection state is now unknown.';
+        break;
+      case 'cellular':
+        connectionMsg = 'You are now connected to a cellular network.';
+        break;
+      case 'wifi':
+        connectionMsg = 'You are now connected to a WiFi network.';
+        break;
+    }
+    Platform.OS === 'ios'
+      ? Alert.alert('Connection change:', connectionMsg)
+      : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
+  }
 
   return (
     <View 
