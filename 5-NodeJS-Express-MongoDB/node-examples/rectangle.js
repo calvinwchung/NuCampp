9a53140f7f756cd81854d29dce38b6e35177fbd3
calvin chung <calvin.w.chung@gmail.com>
2022-08-23 13:18:31 -0400

    changing to ES6

--- 5-NodeJS-Express-MongoDB/node-examples/rectangle.js
+++ 5-NodeJS-Express-MongoDB/node-examples/rectangle.js
@@ -1,4 +1,19 @@
-module.exports = (x, y, callback) => {
+// module.exports = (x, y, callback) => {
+//   if (x <= 0 || y <= 0) {
+//     callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`));
+//   } else {
+//       setTimeout(() => 
+//         callback(null, {
+//           perimeter: () => 2 * (x + y),
+//           area: () => x * y
+//         }),
+//         2000
+//       )
+//   }
+// }
+
+
+const Rect = (x, y, callback) => {
   if (x <= 0 || y <= 0) {
     callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`));
   } else {
@@ -11,5 +26,7 @@ module.exports = (x, y, callback) => {
       )
   }
 }
+
+export default Rect;
 
 
